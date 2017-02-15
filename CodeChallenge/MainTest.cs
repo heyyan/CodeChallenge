6fc469276a41fecc3389623b97d75da06e4f960b
hkhan <hkhan@CRLHL-PSDT40>
2017-02-15 18:31:26 +0500

    Add project files.

---
+++ CodeChallenge/MainTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeChallenge
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
